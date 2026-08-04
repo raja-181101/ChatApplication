5361f9fa5cf40929999bf39081b26bbd085ccd13
Rajapandu_M <rajapandu2024@gmail.com>
2026-08-04 13:27:38 +0530

    colors has been modified and maintained

--- app/src/main/java/com/example/chatapplication/LauncherActivity.java
+++ app/src/main/java/com/example/chatapplication/LauncherActivity.java
@@ -3,6 +3,10 @@ package com.example.chatapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,17 +19,58 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LauncherActivity extends AppCompatActivity {
 
     FirebaseAuth myAuth;
+    private ImageView logo;
+    private TextView appName , tagName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_launcher);
         myAuth = FirebaseAuth.getInstance();
+        logo = findViewById(R.id.logo_id);
+        appName = findViewById(R.id.app_name_id);
+        tagName = findViewById(R.id.tag_name_id);
 
-        if (myAuth.getCurrentUser()!=null){
-            goTOMatch();
-        }else {
-            signInAnonymously();
-        }
+        logo.setAlpha(0f);
+        logo.setScaleX(0.85f);
+        logo.setScaleY(0.85f);
+
+        appName.setAlpha(0f);
+        tagName.setAlpha(0f);
+
+        logo.animate()
+                .alpha(1f)
+                .scaleX(1f)
+                .scaleY(1f)
+                .setDuration(900)
+                .withEndAction(()->{
+                    logo.animate()
+                            .translationY(-30f)
+                            .setDuration(600)
+                            .withEndAction(()->{
+                                appName.animate()
+                                        .alpha(1f)
+                                        .setDuration(700)
+                                        .withEndAction(()->{
+                                            tagName.animate()
+                                                    .alpha(1f)
+                                                    .setDuration(700)
+                                                    .withEndAction(()->{
+                                                        tagName.postDelayed(()->{
+                                                            if (myAuth.getCurrentUser()!=null){
+                                                                goTOMatch();
+                                                            }else {
+                                                                signInAnonymously();
+                                                            }
+
+                                                        },1200);
+
+
+                                                    });
+                                        });
+
+                            });
+                });
+
 
     }
 
